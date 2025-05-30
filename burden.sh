28b6faa14229f461cdeef2b2a556cd406d9a642c
54sym <140576853+54sym@users.noreply.github.com>
2025-05-30 13:41:29 +0800

    Add files via upload

--- burden.sh
+++ burden.sh
@@ -7,5 +7,5 @@ for group in `ls -d ../00.input/*/|awk -F '/' '{print $(NF-1)}'`
 do
     mkdir -p $cwd/$group;cd $cwd/$group
     vcf="../../00.input/$group/${group}.chr${k}.vcf.gz"
-    /usr/bin/python3.6 ../burden.py $vcf ../chr${k}.DamageSnp.txt ../kegg.input.txt $group.chr$k 1>${group}.chr${k}.log 2>&1
+    /usr/bin/python3.6 ../burden.py $vcf ../chr${k}.DamageSnp.txt ../pathway.input.txt $group.chr$k 1>${group}.chr${k}.log 2>&1
 done 
